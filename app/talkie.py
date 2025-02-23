ab806c1ce50cc6b28c086a91f67fa06ec939df64
Marcus Bernal <mabernal2526@gmail.com>
2025-02-22 19:09:51 -0700

    langchain example
    credit: Code With Aaroha

---
+++ app/talkie.py
@@ -0,0 +1,20 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_ollama.llms import OllamaLLM
+import streamlit as st
+
+st.title("Talkie Toaster 🚀")
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+model = OllamaLLM(model="gemma2")
+
+chain = prompt | model
+
+question = st.chat_input("Enter your question here")
+
+if question:
+    st.write(chain.invoke({"question": question}))
