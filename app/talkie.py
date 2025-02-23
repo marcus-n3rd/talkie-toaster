231619dd862e2dd48368ba598413616df0c902ac
Marcus Bernal <mabernal2526@gmail.com>
2025-02-23 14:39:36 -0700

    sanitize for code output, refine personality

--- app/talkie.py
+++ app/talkie.py
@@ -1,9 +1,19 @@
 import yaml
+import re
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_ollama.llms import OllamaLLM
 import streamlit as st
 
-st.title("Talkie Toaster 🚀")
+title = "Talkie Toaster 🚀"
+model_name = "gemma2"
+# deepseek-r1 was fun to watch, but had trash output
+# gemma2 was surprisingly accurate but still off from event gpt-35-turbo
+# qwem2 was too helpful, but good, didn't check accuracy
+# llama3.2 was fast but just as flakey as gemma
+# phi4 was slower but prone to hallucinations, between gemma and deepseek
+
+st.set_page_config(page_title=title)
+st.title(title)
 
 with open("data/talkie.yml") as stream:
     config = yaml.safe_load(stream)
@@ -13,11 +23,27 @@ if "messages" not in st.session_state:
     if (config):
         st.session_state["messages"].extend(config["messages"])
 
+def sanitize_for_code(message):
+    return {
+        "role": message["role"],
+        "content": message["content"].translate(str.maketrans({
+            r"{": r"{{",
+            r"}": r"}}",
+            "-":  r"\-",
+            "]":  r"\]",
+            "\\": r"\\",
+            "^":  r"\^",
+            "$":  r"\$",
+            "*":  r"\*",
+            ".":  r"\.",
+        }))
+    }
+
 messages = []
-messages.extend(st.session_state["messages"])
+messages.extend(map(sanitize_for_code, st.session_state["messages"]))
 messages.append({"role": "user", "content": "{prompt}"})
 prompt = ChatPromptTemplate.from_messages(messages)
-model = OllamaLLM(model="gemma2")
+model = OllamaLLM(model=model_name)
 chain = prompt | model
 
 for msg in st.session_state["messages"]:
