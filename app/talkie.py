db754f6f7176d8444cf44bac0a01fbc9cc2c5b49
Marcus Bernal <mabernal2526@gmail.com>
2025-02-26 18:31:50 -0700

    add quotes

--- app/talkie.py
+++ app/talkie.py
@@ -11,12 +11,21 @@ model_name = "gemma2"
 # qwem2 was too helpful, but good, didn't check accuracy
 # llama3.2 was fast but just as flakey as gemma
 # phi4 was slower but prone to hallucinations, between gemma and deepseek
+config_files = [
+    "data/quotes.yml",
+    "data/talkie.yml",
+]
+chat_input_text = "Ask about anything but bready topics..."
 
 st.set_page_config(page_title=title)
 st.title(title)
 
-with open("data/talkie.yml") as stream:
-    config = yaml.safe_load(stream)
+config = { "messages": [] }
+for config_file in config_files:
+    with open(config_file) as stream:
+        yaml_config = yaml.safe_load(stream)
+        if (yaml_config["messages"]):
+            config["messages"].extend(yaml_config["messages"])
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
@@ -49,7 +58,7 @@ chain = prompt | model
 for msg in st.session_state["messages"]:
     st.chat_message(msg["role"]).write(msg["content"])
 
-if prompt := st.chat_input():
+if prompt := st.chat_input(placeholder=chat_input_text):
     st.chat_message("user").write(prompt)
     st.session_state["messages"].append({"role": "user", "content": prompt})
 
